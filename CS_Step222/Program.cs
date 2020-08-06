a1092df21e68aaa147acfdf19bc8e755e3866fd3
Gavin Kimberlin <guviner@gmail.com>
2020-08-06 06:07:50 -0700

    Create a program using datetime.

--- CS_Step222/Program.cs
+++ CS_Step222/Program.cs
@@ -10,6 +10,11 @@ namespace CS_Step222
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The current time is " + DateTime.Now + "\n\nEnter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DateTime addhours = DateTime.Now.AddHours(number);
+            Console.WriteLine("\nIt will be {0} in {1} hours", addhours, number);
+            Console.ReadLine();
         }
     }
 }
